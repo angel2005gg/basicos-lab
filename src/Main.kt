23dea8aa6ebafc4c1f2cfbc90dcb448a650e56fa
angel2005gg <bonilllapalechormiguelangel@gmail.com>
2024-06-13 11:45:24 -0500

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,74 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+//////LAB 2////
+fun main() {
+    val trafficLightColor = "White"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+}
+
+////////LAB 3//////
+fun main() {
+    val trafficLightColor = "Yellow"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
+fun main() {
+    val trafficLightColor = "Amber"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
+
+fun main() {
+    val x = 3
+
+    when (x) {
+        2 -> println("x is a prime number between 1 and 10.")
+        3 -> println("x is a prime number between 1 and 10.")
+        5 -> println("x is a prime number between 1 and 10.")
+        7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+fun main() {
+    val x = 3
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+///////LAB 4/////
+fun main() {
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+}
+*/
